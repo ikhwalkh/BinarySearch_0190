0518db07cb9b89837bed000987df5abf14c1753d
ikhwalkh <203380321+ikhwalkh@users.noreply.github.com>
2026-04-17 15:02:33 +0700

    bublesort

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -36,3 +36,23 @@ void input()
         cin >> element[i];
     }
 }
+
+
+void bubbleSortArray()
+{
+    int pass = 1;
+    do
+    {
+        for (int j = 0; j < npanjang - 1 - pass; j++)
+        {
+            if (element[j] > element[j + 1])
+            {
+                int temp = element[j];
+                element[j] = element[j + 1];
+                element[j + 1] = temp;
+            }
+        }
+        
+    }
+    
+}
